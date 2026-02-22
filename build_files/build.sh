6d4cdd323a7d2ff6683879ed518c375f526ad0d8
EpixMan <65122296+EpixMan@users.noreply.github.com>
2026-02-22 13:23:42 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #Niri stuff:
-sudo dnf copr enable avengemedia/dms -y
+sudo dnf copr enable avengemedia/dms fedora-43-x86_64 -y
 sudo dnf install niri dms -y
 rpm-ostree install podman-compose fastfetch helix git-credential-libsecret polkit xdg-user-dirs dbus-tools dbus-daemon gnome-keyring pavucontrol google-noto-emoji-fonts gnome-disk-utility gparted wireplumber pipewire pamixer network-manager-applet NetworkManager-openvpn NetworkManager-openconnect pipewire-alsa wlr-randr wlsunset brightnessctl foot fish fontawesome-fonts-all gnome-themes-extra gnome-icon-theme paper-icon-theme breeze-icon-theme papirus-icon-theme kvantum adw-gtk3-theme ffmpeg gstreamer1-vaapi gstreamer1-plugins-bad-free-extras gstreamer1-plugin-libav dms niri -y
 #Removed bpytop
